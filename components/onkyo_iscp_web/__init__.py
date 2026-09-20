192938cb031ca333c3808e5dc73644bff7ee4782
domhit <dominic.hitschel@gmail.com>
2026-09-20 16:26:33 +0200

    Fix ESPUI Git library declaration

--- components/onkyo_iscp_web/__init__.py
+++ components/onkyo_iscp_web/__init__.py
@@ -30,6 +30,7 @@ async def to_code(config):
     cg.add(var.set_title(config[CONF_TITLE]))
 
     cg.add_library(
-        "ESPUI",
-        "https://github.com/s00500/ESPUI.git#1eb5e2ffc4eebf3a854247fd5208a5b88edc051e",
+        name="ESPUI",
+        repository="https://github.com/s00500/ESPUI.git",
+        version="1eb5e2ffc4eebf3a854247fd5208a5b88edc051e",
 )
